a0befd5f2d01d688d575c52723b1508170493f63
japhethca <chidiebere.anyigor@gmail.com>
2020-08-08 21:40:31 +0100

    change to class based views

--- simple_store/apps/store/views.py
+++ simple_store/apps/store/views.py
@@ -1,32 +1,48 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404, Http404, redirect
 from django.http.response import HttpResponseNotFound
 from django.utils.text import slugify
-
+from django.views import View
+from django.views.generic import DetailView
 from simple_store.apps.core.models import Product
 from .forms import SearchForm
 
 
-def home(request):
-    products = Product.objects.all()
-    search_form = SearchForm()
-    context = {"products": products, "search_form": search_form}
-    return render(request, "store/pages/home.html", context=context)
+class HomePage(View):
+    form_class = SearchForm
+    template_name = "store/pages/home.html"
+    # http_method_names = ["GET"]
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        products = Product.objects.all()
+        context = {"products": products, "search_form": form}
+        return render(request, self.template_name, context=context)
 
 
-def product_detail(request, product_slug, product_id):
-    try:
-        int(product_id)
-    except ValueError:
-        raise Http404()
+class ProductDetail(DetailView):
+    template_name = "store/pages/product_details.html"
+    model = Product
+    context_object_name = "product"
+    pk_url_kwarg = "product_id"
 
-    product = get_object_or_404(Product, pk=product_id)
-    if (slug := slugify(product.name)) != product_slug:
-        return redirect("product-details", slug, product.id)
+    def get(self, request, **kwargs):
+        product_slug = kwargs.get("product_slug")
+        product_id = kwargs.get("product_id")
+        try:
+            int(product_id)
+        except ValueError:
+            raise Http404()
 
-    return render(
-        request, "store/pages/product_details.html", context={"product": product}
-    )
+        product = self.get_object()
+        if (slug := slugify(product.name)) != product_slug:
+            return redirect("product-details", slug, product.id)
+
+        return super().get(request, **kwargs)
 
 
-def cart(request):
-    return render(request, "store/pages/cart.html")
+class CartView(View):
+    http_method_names = ["get", "post", "put"]
+    template_name = "store/pages/cart.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
