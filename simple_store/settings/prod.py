5dbf5d0f844ea5016ec1662b76254a6aed71cffe
japhethca <chidiebere.anyigor@gmail.com>
2020-08-21 15:02:35 +0100

    handle image upload to local and s3

--- simple_store/settings/prod.py
+++ simple_store/settings/prod.py
@@ -1,4 +1,20 @@
 from .base import *
 
+DEBUG = False
 
-# Settings Override
+# AWS S3 settings
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = "sstore-static"
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+AWS_LOCATION = "static"
+
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+# for storing uploaded files like images, docs and others
+# DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "simple_store.apps.core.storage_backends.MediaStorage"
+
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
--- simple_store/urls.py
+++ simple_store/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,3 +9,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("api/v1/", include("simple_store.apps.rest.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
