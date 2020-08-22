b29645999c755080012e27573ce313473a4b7b73
japhethca <chidiebere.anyigor@gmail.com>
2020-08-22 16:07:35 +0100

    implement product search
    - make username field in User optional

--- simple_store/apps/core/apps.py
+++ simple_store/apps/core/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from watson import search as watson
 
 
 class CoreConfig(AppConfig):
-    name = 'core'
+    name = "simple_store.apps.core"
+
+    def ready(self):
+        Product = self.get_model("Product")
+        Brand = self.get_model("Brand")
+        Category = self.get_model("Category")
+
+        watson.register(Product)
+        watson.register(Brand)
+        watson.register(Category)
+
--- simple_store/apps/store/views/__init__.py
+++ simple_store/apps/store/views/__init__.py
@@ -3,3 +3,4 @@ from .product import *
 from .profile import *
 from .checkout import *
 from .order import *
+from .search import *
